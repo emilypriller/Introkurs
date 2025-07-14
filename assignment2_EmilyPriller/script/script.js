6a0358fcda295073da9e3f1973e68dd8ccee4701
emily <emily@priller.se>
2025-07-14 14:36:53 +0200

    A2

---
+++ assignment2_EmilyPriller/script/script.js
@@ -0,0 +1,21 @@
+
+let vip = false
+
+let age = 17
+
+if (age >= 18) {
+
+     alert ("access granted")
+
+} else {
+
+    alert ("Access denied")
+}
+
+
+if (vip) {
+    
+    alert ("Welcome in my best VIP customer:)")
+}
+
+
